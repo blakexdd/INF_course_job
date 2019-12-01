1414063ce5e7d289af725303da012e4f536173b7
blakexxxd <blakexxxd@gmail.com>
2019-12-01 18:22:55 +0300

    Added automatic changes check
    Here i added loading information of organization from json file. When we run project, programme automaticly checking for changes in json database and if anyone occurs, it update's data

--- organizations/models.py
+++ organizations/models.py
@@ -4,7 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Organization(models.Model):
+    # creating name of organization
     name = models.CharField(max_length=20)
+
+    # creating year of establishing organiazation
     year_of_est = models.CharField(max_length=10)
+
+    # creating location of organization
+    location = models.CharField(max_length=40, default='St. Petersburg')
+
+    # creating brief description of organization
+    brief_description = models.CharField(max_length=150, default='Some organization')
+
 
 
--- organizations/views.py
+++ organizations/views.py
@@ -1,21 +1,67 @@
 from django.shortcuts import get_object_or_404, render
-from django.template import RequestContext
 from .models import Organization
+import json
+
+# creating organizations from json file
+with open("package.json") as packege:
+    # assigning data from json file
+    # to data variable
+    data = json.load(packege)
+
+    # loading Organiations
+    existing_organizations = Organization.objects.all()
+
+    # creating list of existing organizations
+    existing_orgs_names = []
+    for existing_org in existing_organizations:
+        existing_orgs_names.append(existing_org.name)
+
+    print(existing_orgs_names)
+
+    # going through all organizations in
+    # json data base and checking if we updated
+    # and loaded all organizations
+    # if we don't find name of organizations that placed in json file
+    # at our site, we add this record and if we find a name
+    # we simply update previous object, so each time when we start our
+    # server, organizations data base updates
+    for new_organization in data['organizations']:
+        if new_organization['name'] not in existing_orgs_names:
+            # print(new_organization['name'], 'is not in existing organizations')
+            # if organization doesn't exist, add new organization
+            new_org = Organization(name=new_organization['name'], year_of_est=new_organization['year_of_establishing'],
+                                   location='{} {} {}'.format(new_organization['location']['index'],
+                                                              new_organization['location']['city'],
+                                                              new_organization['location']['adress']))
+            new_org.save()
+        else:
+            # print(new_organization['name'], 'is in EXISTING organizations')
+            # print('new year_of_est: ', new_organization['year_of_establishing'])
+            # if organizations exists, modify it's fields
+            Organization.objects.filter(name=new_organization['name']).update(year_of_est=new_organization['year_of_establishing'],
+                                                                              location='{} {} {}'.format(new_organization['location']['index'],
+                                                             new_organization['location']['city'],
+                                                              new_organization['location']['adress']))
+            #print(Organization.objects.filter(name=new_organization['name']))
 
 
-# Create your views here.
 
+# view function for organizations
 def orgs(request):
     orgs = Organization.objects.all()
     return render(request, 'organizations/organizations.html',
                               {'organizations': orgs})
 
+# view function for particular organization
 def one_org(request, org_id):
+    # assigning Organization object to org variable
     org = get_object_or_404(Organization, pk=org_id)
 
+    # creating dict with organization fields
     vars = dict(
         name = org.name,
-        year_of_est = org.year_of_est
+        year_of_est = org.year_of_est,
+        location = org.location
     )
 
     return render(request, 'organizations/one_org.html', vars)
